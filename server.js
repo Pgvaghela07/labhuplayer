3348e13902fcf68357949168520cc0268cf824d8
Pgvaghela <piyushvaghela223@gmail.com>
2024-02-13 11:44:48 +0530

    add

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const path = require("path");
 const app = express();
 
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-        res.render("/index");
+        res.render("index.ejs");
 });
 
 app.listen(8080, () => {
